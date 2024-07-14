5429786ce2ca3500351ea9a066a8df95eac62474
Shivamshukla001 <shivamshukla6350@gmail.com>
2024-07-14 17:01:03 +0530

    day1 and day2

---
+++ day1.js
@@ -0,0 +1,56 @@
+//task 1
+var a = 5;
+console.log(a);
+
+let bro = 9;
+console.log(bro);
+
+const c = 0.45;
+console.log(c);
+
+//task 2
+let array = [1,2,3,4,5];
+console.log(typeof array);
+
+let a = 34;
+console.log(typeof a);
+let str = "SHivam"
+console.log(typeof str);
+
+let obj = {
+    name: "shivam",
+    age: 13
+}
+
+console.log(typeof obj);
+
+let arr =[1,2,3,4,5];
+console.log(typeof arr); 
+
+//task 3
+let a = 9;
+console.log(a);
+a = 7;
+console.log(a);
+
+const a = 9;
+console.log(a);
+a = 8;
+console.log(a);
+
+//task 4
+let a = 9;
+let orri  = "Shivam"
+console.log(a, typeof a);
+console.log(orri, typeof orri);
+
+//task 5
+let a = 9;
+console.log(a);
+a=7;
+console.log(a);
+
+const b = 6;
+console.log(b);
+b=3;
+console.log(b);
---
+++ day2.js
@@ -0,0 +1,96 @@
+// ARithmetic operator
+// task 1
+let a = 4;
+let b =6;
+console.log(a+b);
+
+// task 2
+let c = 7;
+let d = 5;
+
+console.log(c-d);
+
+// task 3 
+let e = 9;
+let f = 2;
+console.log(e*f);
+
+// task 4
+let g = 4;
+let h = 3;
+console.log(g/h);
+
+// task5
+let i = 5
+let j = 2
+let res = i% j
+console.log(res);
+
+// task 6
+//Assigment operator
+let k = 9;
+console.log(k += 7);
+
+// task 7
+let l = 9;
+console.log( l-=3);
+console.log(l);
+
+
+// task 8
+//comparison operator
+let m = 9;
+let n =8;
+if(m>5 && n>5){
+    console.log("great");
+}else{
+    console.log("ok");
+}
+
+// task 9
+let o = 6;
+let p = 4;
+if(o >= 4 && p<= 2){
+    console.log("yesss");
+}else{
+    console.log("no");
+}
+
+
+// task 10
+let q = 5;
+let r = "5";
+console.log(q==r);
+console.log(q===r);
+
+// task 11
+//logical operator
+let s = 6;
+let t = 4;
+if(s >= 4 && t <= 2){
+    console.log("yesss");
+}else{
+    console.log("no");
+}
+
+// task 12
+let x = 6;
+let y = 4;
+if(x >= 4 || y <= 2){
+    console.log("yesss");
+}else{
+    console.log("no");
+}
+
+// task 13
+let z = 2;
+if(z!=2){
+    console.log("yess");
+}console.log("no");
+
+// task 14
+// condition ? trueExpression : falseExpression
+let abc = 4;
+let result = (abc>7)? true:  false
+console.log(result);
+
