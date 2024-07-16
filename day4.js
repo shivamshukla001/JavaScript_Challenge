03edcb122c95e5bb8e5e3c0c971f24e44ce6a749
Shivamshukla001 <shivamshukla6350@gmail.com>
2024-07-16 21:07:50 +0530

    all about day 4

---
+++ day4.js
@@ -0,0 +1,75 @@
+task1
+for(let i=0; i<=10; i++){
+    console.log(i);
+}
+
+task2
+for(let i=1; i<=10; i++){
+    console.log(i*5);
+}
+
+task3
+let i =0;
+while (i<=10) {
+    console.log(i=(i+1));
+    i++;
+
+}
+
+task4;
+let num = 10;
+while (!num<=0) {
+    console.log(num);
+    num--;
+}
+
+
+task5
+let r =1
+do {
+    console.log(r);
+    r++
+} while (r<=5);
+
+
+task6
+let number = 6;
+let result = 1;
+let i = number;
+
+if (number === 0) {
+    result = 1;
+} else {
+    do {
+        result *= i;
+        i--;
+    } while (i > 0);
+}
+
+console.log(`The factorial of ${number} is ${result}`);
+
+task7
+let rows;
+let column;
+for(rows=1; rows<=5; rows++){
+    for(column=1; column<=5; column++){
+        console.log("*", " ")
+    }
+}
+
+task8
+for(let i =1; i<=10; i++){
+    if(i==5){
+        continue;
+       
+}
+console.log(i);
+}
+
+task9 
+for(let i = 1; i<=10; i++){
+    if(i==7) break;
+    console.log(i);
+        
+
+}
