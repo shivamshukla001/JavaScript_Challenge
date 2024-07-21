e4ad7aabe20477ee0eb972fc38eff692e5fd6fc5
Shivamshukla001 <shivamshukla6350@gmail.com>
2024-07-21 17:31:27 +0530

    day6

---
+++ day6.js
@@ -0,0 +1,54 @@
+// let number = [1,2,3,4,5];
+// console.log(number);
+
+//task2
+// let arr=[1,2,3,4,5];
+// console.log(arr[0]+ " "+ arr[4]);
+
+//task3
+
+let newarr=[1,2,3,4,5];
+// newarr.push(6);
+// console.log(newarr);
+// newarr.pop()
+// console.log(newarr);
+// newarr.shift()
+// console.log(newarr);
+// newarr.unshift(0)
+// console.log(newarr);
+
+
+//task4
+let originalArr = [1,3,4,5,6];
+//  let newd = originalArr.map(Math.sqrt(originalArr))
+
+// console.log(newd);
+
+//task5
+
+let ages = [12,45,67,34,56,22];
+let year = ages.filter(group);
+
+function group(age){
+    return age <= 20;
+}
+
+let newarray = [1,2,3,4,5,6,7,8];
+for(let i =0 ; i<newarray.length; i++){
+    // console.log(newarray[i]);
+}
+
+let newdata =[2,5,7,9,1,,6,9];
+newdata.forEach(element => {
+    // console.log(element + " ");
+}); 
+
+
+
+const make = [[1,2,3],[4,5,6],[7,8,9]]
+console.log(make);
+console.log(make[0][1]);
+
+console.log(make[2][2]);
+
+
